f4ccdd6a08dd13476c6a5ba2242a36c071723d5e
LDF913 <ldf913@gmail.com>
2021-06-22 17:47:42 -0400

    created app from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text(
+                'Ask Me Anything',
+              ),
+            ),
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int mbAnswer = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      mbAnswer = Random().nextInt(5) + 1;
+                    });
+                  },
+                  child: Image.asset(
+                    'images/ball$mbAnswer.png',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
